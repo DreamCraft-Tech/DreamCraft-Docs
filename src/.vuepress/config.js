574ada057f9a65e67b3634fd4c52169f7863cdfc
晓宇致梦Universe <53889721+DreamUniverse843@users.noreply.github.com>
2022-07-02 14:44:09 +0800

    Change config files to JavaScript & Import Mathjax

---
+++ src/.vuepress/config.js
@@ -0,0 +1,60 @@
+module.exports = {
+    title : 'DreamCraft Docs',
+    description : 'DreamCraft Docs',
+    evergreen : true,
+    base : '/',
+    locales: {
+        '/':{
+            lang : 'zh-CN',
+            title : 'DreamCraft 用户手册',
+        },
+        '/en/':{
+            lang : 'en-US',
+            title : 'DreamCraft Docs'
+        }
+    },
+    themeConfig :{
+        repo : 'DreamCraft-Tech/DreamCraft-Docs',
+        docsBranch : 'master',
+        docsDir : 'src',
+        editLinks : true,
+        locales :{
+            '/':{
+                selectText : '选择语言',
+                label : '简体中文',
+                docsDir : 'src',
+                editLinkText : '在 Github 上提交更改',
+                lastUpdated : '最后更新',
+                sidebar:{
+                    'mirrors': [
+                        '/mirrors/introduction',
+                    ],
+                    'launcher': [
+                        '/launcher/introduction',
+                    ],
+                    '/': [
+                        '/introduction',
+                    ]
+                },
+                nav:[
+                    {
+                        text:'Services',
+                        ariaLabel:'Services Menu',
+                        items:[
+                            { text:'Web Service',link:'/'},
+                            { text:'Launcher',link:'/launcher/'},
+                            { text:'Mirrors',link:'/mirrors/'}
+                        ]
+                    }
+                ]
+            },
+            '/en/':{
+                selectText : 'Language',
+                label : 'English',
+                docsDir : 'src/en',
+                editLinkText : 'Commit Changes on Github',
+                lastUpdated : 'Last updated on'
+            }
+        }
+    }
+  }
